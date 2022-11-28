377f8af2c249ad8b1a1da0f2f09eed05fcbc1897
hijimo <bakatxgt@gmail.com>
2022-11-28 14:37:26 +0800

    1

--- src/utils/authorize.ts
+++ src/utils/authorize.ts
@@ -1,29 +1,29 @@
 import {
   getSetting,
   authorize as taroAuthorize,
-  getUserInfo as taroGetUserInfo,
+  getUserProfile as taroGetUserInfo,
   login,
   getLocation as taroGetLocation,
-} from '@tarojs/taro';
+} from "@tarojs/taro";
 
 export enum ScopeEnums {
-  userInfo = 'scope.userInfo', // 是否授权用户信息，对应接口 wx.getUserInfo
+  userInfo = "scope.userInfo", // 是否授权用户信息，对应接口 wx.getUserInfo
 
-  location = 'scope.userLocation', // 是否授权地理位置，对应接口 wx.getLocation, wx.chooseLocation
-  address = 'scope.address', // 是否授权通讯地址，对应接口 wx.chooseAddress
-  invoiceTitle = 'scope.invoiceTitle', // 是否授权发票抬头，对应接口 wx.chooseInvoiceTitle
-  invoice = 'scope.invoice', // 是否授权获取发票，对应接口 wx.chooseInvoice
-  werun = 'scope.werun', // 是否授权微信运动步数，对应接口 wx.getWeRunData
-  record = 'scope.record', // 是否授权录音功能，对应接口 wx.startRecord
-  writePhotosAlbum = 'scope.writePhotosAlbum', // 是否授权保存到相册 wx.saveImageToPhotosAlbum, wx.saveVideoToPhotosAlbum
-  camera = 'scope.camera', // 是否授权摄像头，对应[camera]((camera)) 组件
+  location = "scope.userLocation", // 是否授权地理位置，对应接口 wx.getLocation, wx.chooseLocation
+  address = "scope.address", // 是否授权通讯地址，对应接口 wx.chooseAddress
+  invoiceTitle = "scope.invoiceTitle", // 是否授权发票抬头，对应接口 wx.chooseInvoiceTitle
+  invoice = "scope.invoice", // 是否授权获取发票，对应接口 wx.chooseInvoice
+  werun = "scope.werun", // 是否授权微信运动步数，对应接口 wx.getWeRunData
+  record = "scope.record", // 是否授权录音功能，对应接口 wx.startRecord
+  writePhotosAlbum = "scope.writePhotosAlbum", // 是否授权保存到相册 wx.saveImageToPhotosAlbum, wx.saveVideoToPhotosAlbum
+  camera = "scope.camera", // 是否授权摄像头，对应[camera]((camera)) 组件
 }
 
 // 微信登录，获取一次性临时code
 export async function wxLogin() {
   const result = await login();
   if (!result.code) {
-    throw new Error('wx login no code');
+    throw new Error("wx login no code");
   }
   return result.code;
 }
@@ -32,9 +32,11 @@ export async function wxLogin() {
 export async function isAuthorizationUserInfo() {
   const hasAuthorize = await checkAuthorize(ScopeEnums.userInfo);
   if (!hasAuthorize) {
-    throw new Error('no authorize');
+    throw new Error("no authorize");
   }
-  const userInfo = await taroGetUserInfo();
+  const userInfo = await taroGetUserInfo({
+    desc: "为了向您的好友展示您的头像和昵称",
+  });
   return userInfo;
 }
 
@@ -55,13 +57,15 @@ export async function authorize(scope: string) {
 
 export async function getUserInfo() {
   await authorize(ScopeEnums.userInfo);
-  const userInfo = await taroGetUserInfo();
+  const userInfo = await taroGetUserInfo({
+    desc: "为了向您的好友展示您的头像和昵称",
+  });
   return userInfo;
 }
 
 // 获取地理位置
 export async function getLocation() {
   await authorize(ScopeEnums.location);
-  const location = await taroGetLocation({ type: 'gcj02' });
+  const location = await taroGetLocation({ type: "gcj02" });
   return location;
 }
